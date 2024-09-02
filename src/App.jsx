aebc9378eb82d509dba95412ffc2414c87b023a8
Bengi994 <88071990+Bengi994@users.noreply.github.com>
2024-09-02 12:41:22 -0500

    hito 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import './App.css'
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import Footer from './components/Footer';
-import RegisterPage from './components/RegisterPage';
-import LoginPage from './components/LoginPage';
-import Cart from "./components/Cart";
+import "./App.css";
+import Navbar from "./components/Navbar";
+/* import Home from "./components/Home"; */
+import Footer from "./components/Footer";
+/* import RegisterPage from "./components/RegisterPage";
+import LoginPage from "./components/LoginPage";
+import Cart from "./components/Cart"; */
+import Pizza from "./components/Pizza";
 
 function App() {
   return (
@@ -12,11 +13,12 @@ function App() {
       <Navbar />
       {/* <Home /> */}
       {/* <RegisterPage /> */}
-     {/*  <LoginPage /> */}
-      <Cart />
+      {/*  <LoginPage /> */}
+      {/* <Cart /> */}
+      <Pizza />
       <Footer />
     </>
-  )
+  );
 }
 
-export default App 
+export default App;
