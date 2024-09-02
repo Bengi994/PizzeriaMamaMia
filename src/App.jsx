b29ef245e426a74b5c3276faae456d9618c1a135
Bengi994 <88071990+Bengi994@users.noreply.github.com>
2024-09-02 11:58:08 -0500

    hito 3

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './components/Home';
 import Footer from './components/Footer';
 import RegisterPage from './components/RegisterPage';
 import LoginPage from './components/LoginPage';
+import Cart from "./components/Cart";
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       <Navbar />
       {/* <Home /> */}
       {/* <RegisterPage /> */}
-      <LoginPage />
+     {/*  <LoginPage /> */}
+      <Cart />
       <Footer />
     </>
   )
--- src/components/CardPizza.jsx
+++ src/components/CardPizza.jsx
@@ -1,42 +1,39 @@
-const MyCard = ({ name, price, ingredients, img }) => {
+const CardPizza = ({ name, price, ingredients, img }) => {
   return (
-    <>
-      <div class="card-group">
-        <div class="card" style={{ maxWidth: "350px", margin: "10px" }}>
-          <img class="card-img-top" src={img} alt={name} />
-          <div class="card-body">
-            <h5 class="card-title">{name}</h5>
-            <div className="m-2">
-              <p class="card-text text-center">Ingredientes:</p>
-              <p className="card-text text-center">
-                🍕
-                {ingredients.map((ingredient, index) => (
-                  <span
-                    key={index}
-                    style={{ display: "inline-block", marginRight: "5px" }}
-                  >
-                    <span role="img" aria-label="ingredient"></span>{" "}
-                    {ingredient},
-                  </span>
-                ))}
-              </p>
-            </div>
-
-            <p className="card-text text-center fs-4 fw-bold">
-              Precio: ${price.toLocaleString()}
+    <div className="col-md-4 d-flex align-items-stretch mb-4 mt-4">
+      <div className="card" style={{ maxWidth: "400px", margin: "auto" }}>
+        <img className="card-img-top" src={img} alt={name} />
+        <div className="card-body">
+          <h5 className="card-title">{name}</h5>
+          <div className="m-2">
+            <p className="card-text text-center">Ingredientes:</p>
+            <p className="card-text text-center">
+              🍕
+              {ingredients.map((ingredient, index) => (
+                <span
+                  key={index}
+                  style={{ display: "inline-block", marginRight: "5px" }}
+                >
+                  <span role="img" aria-label="ingredient"></span> {ingredient},
+                </span>
+              ))}
             </p>
-            <div className="d-flex justify-content-between mt-4 mx-4">
-              <a href="#" className="btn btn-outline-secondary">
-                Ver Más 👀
-              </a>
-              <a href="#" className="btn text-white bg-black">
-                Añadir 🛒
-              </a>
-            </div>
+          </div>
+
+          <p className="card-text text-center fs-4 fw-bold">
+            Precio: ${price.toLocaleString()}
+          </p>
+          <div className="d-flex justify-content-between mt-4 mx-4">
+            <a href="#" className="btn btn-outline-secondary">
+              Ver Más 👀
+            </a>
+            <a href="#" className="btn text-white bg-black">
+              Añadir 🛒
+            </a>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
-export default MyCard;
+export default CardPizza;
